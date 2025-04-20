d62ceac2c21118c014935ebfeb061a55e55e56c0
Ryan <ryan32111@walla.co.il>
2025-04-20 18:44:17 +0300

    Backend init

---
+++ src/main/kotlin/com/math_problem_server/MathProblemServerApplication.kt
@@ -0,0 +1,11 @@
+package com.math_problem_server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MathProblemServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<MathProblemServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/math_problem_server/MathProblemServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.math_problem_server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MathProblemServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
